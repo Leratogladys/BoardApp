0a1bd89df6c78e63d5155f39744acf3543e564ef
Lerato Molefe <137295153+Leratogladys@users.noreply.github.com>
2026-08-22 18:51:16 +0200

    Task 2.2-2.3: Board model class with properties, constructors, ToString

---
+++ BoardApp/Models/Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BoardApp.Models;
+
+public class Board
+{
+	public string BoardCode {  get; set; }
+	public string Make {  get; set; }
+	public string Model { get; set; }
+	public int FlashKb { get; set; }
+	public decimal Price { get; set; }
+
+	public Board () { }
+
+	public Board (string boardCode,  string make, string model , int flashKb, decimal price)
+	{
+		BoardCode = boardCode;
+		Make = make;							
+		Model = model;
+		FlashKb = flashKb;
+		Price = price;
+	}
+
+
+	public override string ToString()
+	{
+		return $"{BoardCode}: {Make} {Model} with {FlashKb} KB flash at R{Price:0.00}";
+	}
+}
