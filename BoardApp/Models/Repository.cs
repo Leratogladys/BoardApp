e665768616b78cc31d10bfc66729ebf8d370915c
Lerato Molefe <137295153+Leratogladys@users.noreply.github.com>
2026-08-23 18:58:11 +0200

    Task 6.1-6.2: capture form with tag helpers

---
+++ BoardApp/Models/Repository.cs
@@ -0,0 +1,14 @@
+﻿namespace BoardApp.Models
+{
+    public static class Repository
+    {
+        private static List<Board> boards = new List<Board>();
+        public static IEnumerable<Board> Boards { get { return boards; } }
+
+
+        public static void AddBoard(Board board)
+        {
+            boards.Add(board);
+        }
+    }
+}
